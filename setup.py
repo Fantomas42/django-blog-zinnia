6acc2ddd455508f5ec02a1b3780eb5c95ef07f4c
Fantomas42 <fantomas42@gmail.com>
2010-01-26 20:34:30 +0100

    finishing packaging of the app

--- setup.py
+++ setup.py
@@ -5,27 +5,30 @@ version = '0.1'
 
 setup(name='django-blog-zinnia',
       version=version,
+      
       description='A clear and powerfull weblog application powered with Django',
       long_description=open(os.path.join('README.rst')).read(),
       keywords='django, blog',
+
+      author='Fantomas42',
+      author_email='fantomas42@gmail.com',
+      url='http://github.com/Fantomas42/django-blog-zinnia',
+
+      packages=['zinnia'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
           'Programming Language :: Python',
           'Intended Audience :: Developers',
           'Operating System :: OS Independent',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          ],
-      
-      author='Fantomas42',
-      author_email='fantomas42@gmail.com',
-      url='http://github.com/Fantomas42/django-blog-zinnia',
-      
-      license='GPL',
-      packages=['zinnia'],
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',],
+            
+      license='BSD License',
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-          #
-      ])
+      install_requires=['django-tagging',
+                        'akismet',
+                        ])
+
 
