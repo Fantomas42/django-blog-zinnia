03654eccb7c1e2309fffc6b3adf184e7ba316cb8
Fantomas42 <fantomas42@gmail.com>
2010-05-05 11:53:19 +0200

    adding module information

---
+++ zinnia/__init__.py
@@ -0,0 +1,9 @@
+"""Zinnia"""
+__version__ = '0.5'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'http://github.com/Fantomas42/django-blog-zinnia'
+
