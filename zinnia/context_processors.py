3366dde66d00dd83bea2827a33359fe4e0c34e3b
Fantomas42 <fantomas42@gmail.com>
2010-05-05 12:03:25 +0200

    adding context_processor for zinnia version

--- zinnia/context_processors.py
+++ zinnia/context_processors.py
@@ -1,6 +1,11 @@
 """Context Processors for zinnia"""
+from zinnia import __version__
 from zinnia.settings import MEDIA_URL
 
 def media(request):
     """Adds media-related context variables to the context"""
     return {'ZINNIA_MEDIA_URL': MEDIA_URL}
+
+def version(request):
+    """Adds version of Zinnia to the context"""
+    return {'ZINNIA_VERSION': __version__}
