e4833e793ac57cdfcb32203aa3367eedfceb9b64
Fantomas42 <fantomas42@gmail.com>
2010-05-14 12:31:19 +0200

    adding try except for old version of cms

--- zinnia/plugins/settings.py
+++ zinnia/plugins/settings.py
@@ -1,14 +1,17 @@
 """Settings of Zinnia.plugins"""
 from django.conf import settings
 
-from zinnia.plugins.menu import EntryMenu
-from zinnia.plugins.menu import TagMenu
-from zinnia.plugins.menu import AuthorMenu
-from zinnia.plugins.menu import CategoryMenu
-
-
-APP_MENUS = getattr(settings, 'ZINNIA_APP_MENUS', [EntryMenu, CategoryMenu,
-                                                   TagMenu, AuthorMenu])
 PLUGINS_TEMPLATES = getattr(settings, 'ZINNIA_PLUGINS_TEMPLATES', [])
+try:
+    from zinnia.plugins.menu import EntryMenu
+    from zinnia.plugins.menu import TagMenu
+    from zinnia.plugins.menu import AuthorMenu
+    from zinnia.plugins.menu import CategoryMenu
+
+    APP_MENUS = getattr(settings, 'ZINNIA_APP_MENUS', [EntryMenu, CategoryMenu,
+                                                       TagMenu, AuthorMenu])
+except ImportError:
+    APP_MENUS = []
+
 
 
