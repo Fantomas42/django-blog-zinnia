7befd7c15c81f5d6c699816cbaf9120e1845f82b
Fantomas42 <fantomas42@gmail.com>
2010-05-15 14:00:23 +0200

    finishing zbreadcrumbs

--- zinnia/templatetags/zbreadcrumbs.py
+++ zinnia/templatetags/zbreadcrumbs.py
@@ -1,4 +1,7 @@
 """Breadcrumb module for Zinnia templatetags"""
+import re
+from datetime import datetime
+
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 
@@ -7,6 +10,25 @@ class Crumb(object):
     def __init__(self, name, url=None):
         self.name = name
         self.url = url
+
+def year_crumb(datetime):
+    year = datetime.strftime('%Y')
+    return Crumb(year, reverse('zinnia_entry_archive_year',
+                               args=[year]))
+
+def month_crumb(datetime):
+    year = datetime.strftime('%Y')
+    month = datetime.strftime('%m')
+    month_text = datetime.strftime('%b').capitalize()
+    return Crumb(month_text, reverse('zinnia_entry_archive_month',
+                                     args=[year, month]))
+
+def day_crumb(datetime):
+    year = datetime.strftime('%Y')
+    month = datetime.strftime('%m')
+    day = datetime.strftime('%d')
+    return Crumb(day, reverse('zinnia_entry_archive_day',
+                              args=[year, month, day]))
 
 zinnia_root_url = reverse('zinnia_entry_archive_index')
 
@@ -18,10 +40,13 @@ categories_crumb = Crumb(_('Categories'), reverse('zinnia_category_list'))
 MODEL_BREADCRUMBS = {'Tag': lambda x: [tags_crumb, Crumb(x.name)],
                      'User': lambda x: [authors_crumb, Crumb(x.username)],    
                      'Category': lambda x: [categories_crumb, Crumb(x.title)],
-                     'Entry': lambda x: [Crumb(x.categories.all()[0].title,
-                                               x.categories.all()[0].get_absolute_url()),
-                                         Crumb(x.title)],
-                     }
+                     'Entry': lambda x: [year_crumb(x.creation_date),
+                                         month_crumb(x.creation_date),
+                                         day_crumb(x.creation_date),
+                                         Crumb(x.title)],}
+
+DATE_REGEXP = re.compile(r'.*(?P<year>\d{4})/(?P<month>\d{2})?/(?P<day>\d{2})?.*')
+
 
 def retrieve_breadcrumbs(path, model_instance, root_name=''):
     """Build a semi-hardcoded breadcrumbs
@@ -37,11 +62,25 @@ def retrieve_breadcrumbs(path, model_instance, root_name=''):
             breadcrumbs.extend(MODEL_BREADCRUMBS[key](model_instance))
             return breadcrumbs
 
-    url_components = [comp for comp in path.split('/') if comp]
-    if len(url_components) == 2:#Need to be changed
+    date_match = DATE_REGEXP.match(path)
+    if date_match:
+        date_dict = date_match.groupdict()
+        path_date = datetime(
+            int(date_dict['year']),
+            date_dict.get('month') is not None and int(date_dict.get('month')) or 1,
+            date_dict.get('day') is not None and int(date_dict.get('day')) or 1)
+        
+        date_breadcrumbs = [year_crumb(path_date)]
+        if date_dict['month']: date_breadcrumbs.append(month_crumb(path_date))
+        if date_dict['day']: date_breadcrumbs.append(day_crumb(path_date))
+        breadcrumbs.extend(date_breadcrumbs)
+        
+        return breadcrumbs
+
+    url_components = [comp for comp in
+                      path.replace(zinnia_root_url, '').split('/') if comp]
+    if len(url_components):
         breadcrumbs.append(Crumb(_(url_components[-1].capitalize())))
-    elif len(url_components) > 2:#Idem
-        breadcrumbs.append(archives_crumb)
 
     return breadcrumbs
     
