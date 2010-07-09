18d76adf3c9909b4fb62746520ba7ba9550c77df
Fantomas42 <fantomas42@gmail.com>
2010-07-09 10:46:52 +0200

    adding comment for deleting comments flagged as spam

---
+++ zinnia/management/commands/spam_cleanup.py
@@ -0,0 +1,27 @@
+"""Spam cleanup command module for Zinnia"""
+from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import NoArgsCommand
+
+from zinnia.models import Entry
+
+class Command(NoArgsCommand):
+    """Command object for removing comments
+    flagged as spam"""
+    help = "Remove entry's comments flagged as spam."
+
+    def handle_noargs(self, **options):
+        verbosity = int(options.get('verbosity', 1))
+
+        ct = ContentType.objects.get_for_model(Entry)
+        spams = Comment.objects.filter(is_public=False,
+                                       content_type=ct,
+                                       flags__flag='spam')
+        spams_count = spams.count()
+        spams.delete()
+
+        if verbosity:
+            print '%i spam comments deleted.' % spams_count
+
+
+
