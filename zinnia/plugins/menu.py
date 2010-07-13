1bdbfaee4ba0d235433f2649707b4670914fc9de
nl_0 <nl_0@mail.ru>
2010-07-13 17:05:44 +0800

    EntryMenu hiding is now optional (ZINNIA_HIDE_ENTRY_MENU).

--- zinnia/plugins/menu.py
+++ zinnia/plugins/menu.py
@@ -12,16 +12,17 @@ from zinnia.models import Entry
 from zinnia.models import Category
 from zinnia.managers import tags_published
 from zinnia.managers import authors_published
+from zinnia.plugins.settings import HIDE_ENTRY_MENU
 
 
 class EntryMenu(CMSAttachMenu):
     """Menu for the entries organized by archives dates"""
     name = _('Zinnia Entry Menu')
-    
+
     def get_nodes(self, request):
         nodes = []
         archives = []
-        attributes = {'hidden': True}
+        attributes = {'hidden': HIDE_ENTRY_MENU}
         for entry in Entry.published.all():
             year = entry.creation_date.strftime('%Y')
             month = entry.creation_date.strftime('%m')
@@ -31,12 +32,12 @@ class EntryMenu(CMSAttachMenu):
             key_archive_year = 'year-%s' % year
             key_archive_month = 'month-%s-%s' % (year, month)
             key_archive_day = 'day-%s-%s-%s' % (year, month, day)
-            
+
             if not key_archive_year in archives:
                 nodes.append(NavigationNode(year, reverse('zinnia_entry_archive_year', args=[year]),
                                             key_archive_year, attr=attributes))
                 archives.append(key_archive_year)
-                
+
             if not key_archive_month in archives:
                 nodes.append(NavigationNode(month_text, reverse('zinnia_entry_archive_month', args=[year, month]),
                                             key_archive_month, key_archive_year, attr=attributes))
@@ -46,7 +47,7 @@ class EntryMenu(CMSAttachMenu):
                 nodes.append(NavigationNode(day, reverse('zinnia_entry_archive_day', args=[year, month, day]),
                                             key_archive_day, key_archive_month, attr=attributes))
                 archives.append(key_archive_day)
-                
+
             nodes.append(NavigationNode(entry.title, entry.get_absolute_url(),
                                         entry.pk, key_archive_day))
         return nodes
@@ -54,7 +55,7 @@ class EntryMenu(CMSAttachMenu):
 class CategoryMenu(CMSAttachMenu):
     """Menu for the categories"""
     name = _('Zinnia Category Menu')
-    
+
     def get_nodes(self, request):
         nodes = []
         nodes.append(NavigationNode(_('Categories'), reverse('zinnia_category_list'),
@@ -67,7 +68,7 @@ class CategoryMenu(CMSAttachMenu):
 class AuthorMenu(CMSAttachMenu):
     """Menu for the authors"""
     name = _('Zinnia Author Menu')
-    
+
     def get_nodes(self, request):
         nodes = []
         nodes.append(NavigationNode(_('Authors'), reverse('zinnia_author_list'),
@@ -81,7 +82,7 @@ class AuthorMenu(CMSAttachMenu):
 class TagMenu(CMSAttachMenu):
     """Menu for the tags"""
     name = _('Zinnia Tag Menu')
-    
+
     def get_nodes(self, request):
         nodes = []
         nodes.append(NavigationNode(_('Tags'), reverse('zinnia_tag_list'),
@@ -95,7 +96,7 @@ class TagMenu(CMSAttachMenu):
 class EntryModifier(Modifier):
     """Menu Modifier for entries,
     hide the MenuEntry in navigation, not in breadcrumbs"""
-    
+
     def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
         if breadcrumb:
             return nodes
@@ -103,7 +104,7 @@ class EntryModifier(Modifier):
             if node.attr.get('hidden'):
                 nodes.remove(node)
         return nodes
-            
+
 
 menu_pool.register_menu(EntryMenu)
 menu_pool.register_menu(CategoryMenu)
