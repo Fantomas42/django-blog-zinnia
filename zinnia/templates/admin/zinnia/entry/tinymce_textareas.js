0ec9b7bdff8ce368da7a937f322f4c453c79905c
Fantomas42 <fantomas42@gmail.com>
2010-08-05 17:07:54 +0200

    rewriting configuration of tinymce

--- zinnia/templates/admin/zinnia/entry/tinymce_textareas.js
+++ zinnia/templates/admin/zinnia/entry/tinymce_textareas.js
@@ -1,9 +1,12 @@
 tinyMCE.init({
-  mode: "textareas",
-  theme: "advanced",
-  plugins: "spellchecker,directionality,paste,searchreplace",
-  language: "{{ language }}",
-  directionality: "{{ directionality }}",
-  spellchecker_languages : "{{ spellchecker_languages }}",
-  spellchecker_rpc_url : "{{ spellchecker_rpc_url }}"
-});
+	mode: "exact",
+	elements: "id_content",
+	theme: "advanced",
+	theme_advanced_toolbar_location : "top",
+	height: "300",
+	plugins: "spellchecker,directionality,paste,searchreplace",
+	language: "{{ language }}",
+	directionality: "{{ directionality }}",
+	spellchecker_languages : "{{ spellchecker_languages }}",
+	spellchecker_rpc_url : "{{ spellchecker_rpc_url }}"
+	});
