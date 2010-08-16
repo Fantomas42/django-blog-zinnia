6bfd02fd83da14d045fc01d16783942b14c51825
Fantomas42 <fantomas42@gmail.com>
2010-08-16 10:50:49 +0200

    fix issue #11

--- zinnia/ping.py
+++ zinnia/ping.py
@@ -21,7 +21,7 @@ class DirectoryPinger(object):
 
         if not blog_url or not blog_feed:
             blog_url = '%s%s' % (site, reverse('zinnia_entry_archive_index'))
-            blog_feed = '%s%s' % (site, reverse('zinnia_feeds', args=['latest', ]))
+            blog_feed = '%s%s' % (site, reverse('zinnia_entry_latest_feed'))
 
     def ping(self, entry):
         entry_url = '%s%s' % (site, entry.get_absolute_url())
