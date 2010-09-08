355253d4cab434de14faacd7521db710cc8d9c65
Fantomas42 <fantomas42@gmail.com>
2010-09-08 16:19:28 +0200

    fixing feeds for categories

--- zinnia/views/categories.py
+++ zinnia/views/categories.py
@@ -1,22 +1,19 @@
 """Views for zinnia categories"""
-from django.http import Http404
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_list
 
 from zinnia.models import Category
 from zinnia.settings import PAGINATION
 
+
+def get_category_or_404(path):
+    """Retrieve a Category by a path"""
+    path_bits = [p for p in path.split('/') if p]
+    return get_object_or_404(Category, slug=path_bits[-1])
+
 def category_detail(request, path, page=None):
     """Display the entries of a category"""
-
-    path_bits = [p for p in path.split('/') if p]
-
-    category = None
-    for cat in get_list_or_404(Category, slug=path_bits[-1]):
-        if cat.tree_path in path:
-            category = cat
-    if not category:
-        raise Http404
+    category = get_category_or_404(path)
 
     return object_list(request, queryset=category.entries_published_set(),
                        paginate_by=PAGINATION, page=page,
