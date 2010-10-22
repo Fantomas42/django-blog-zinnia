0f361e49515f09a257b0f0a415d85a45d2c23788
Fantomas42 <fantomas42@gmail.com>
2010-10-22 14:55:03 +0200

    fixing import of zinnia in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-import zinnia
+import os
+import sys
 from datetime import date
+
+sys.path.append(os.path.abspath('..'))
+import zinnia
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
