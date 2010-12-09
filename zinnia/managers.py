14b7c32b69e76f096dc11ff827a3ebd7ad57e029
Tobias von Klipstein <tobias@uxebu.com>
2010-12-10 00:39:57 +0100

    Using PUBLISHED instead of 2. Doh!

--- zinnia/managers.py
+++ zinnia/managers.py
@@ -20,7 +20,7 @@ def tags_published():
 def authors_published():
     """Return the published authors"""
     from django.contrib.auth.models import User
-    return User.objects.filter(entry__status=2).distinct()
+    return User.objects.filter(entry__status=PUBLISHED).distinct()
 
 def entries_published(queryset):
     """Return only the entries published"""
