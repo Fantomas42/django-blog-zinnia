69cff16be3e16f12efe4025052f936110b45442a
Fantomas42 <fantomas42@gmail.com>
2011-04-04 15:24:52 +0200

    updating tests

--- zinnia/tests/__init__.py
+++ zinnia/tests/__init__.py
@@ -3,22 +3,23 @@ from unittest import TestSuite
 from unittest import TestLoader
 from django.conf import settings
 
-from zinnia.tests.entry import EntryTestCase
-from zinnia.tests.entry import EntryHtmlContentTestCase
+from zinnia.tests.entry import EntryTestCase  # ~0.2s
+from zinnia.tests.entry import EntryHtmlContentTestCase  # ~0.5s
 from zinnia.tests.entry import EntryGetBaseModelTestCase
 from zinnia.tests.category import CategoryTestCase
-from zinnia.tests.managers import ManagersTestCase
-from zinnia.tests.feeds import ZinniaFeedsTestCase
-from zinnia.tests.views import ZinniaViewsTestCase  # ~3.5s ouch...
-from zinnia.tests.pingback import PingBackTestCase  # ~0.9s
+from zinnia.tests.managers import ManagersTestCase  # ~1.2s
+from zinnia.tests.feeds import ZinniaFeedsTestCase  # ~0.4s
+from zinnia.tests.views import ZinniaViewsTestCase  # ~3s ouch...
+from zinnia.tests.pingback import PingBackTestCase  # ~0.3s
 from zinnia.tests.metaweblog import MetaWeblogTestCase  # ~0.6s
 from zinnia.tests.comparison import ComparisonTestCase
-from zinnia.tests.quick_entry import QuickEntryTestCase
-from zinnia.tests.sitemaps import ZinniaSitemapsTestCase
+from zinnia.tests.quick_entry import QuickEntryTestCase  # ~0.4s
+from zinnia.tests.sitemaps import ZinniaSitemapsTestCase  # ~0.3s
 from zinnia.tests.ping import ExternalUrlsPingerTestCase
-from zinnia.tests.templatetags import TemplateTagsTestCase
-from zinnia.tests.moderator import EntryCommentModeratorTestCase
+from zinnia.tests.templatetags import TemplateTagsTestCase  # ~0.4s
+from zinnia.tests.moderator import EntryCommentModeratorTestCase  # ~0.1s
 from zinnia.signals import disconnect_zinnia_signals
+# TOTAL ~ 7.9s
 
 
 def suite():
--- zinnia/tests/entry.py
+++ zinnia/tests/entry.py
@@ -21,15 +21,11 @@ class EntryTestCase(TestCase):
     def setUp(self):
         params = {'title': 'My entry',
                   'content': 'My content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-entry'}
         self.entry = Entry.objects.create(**params)
-        self.author = User.objects.create_user(username='webmaster',
-                                               email='webmaster@example.com')
 
     def test_discussions(self):
         site = Site.objects.get_current()
-
         self.assertEquals(self.entry.discussions.count(), 0)
         self.assertEquals(self.entry.comments.count(), 0)
         self.assertEquals(self.entry.pingbacks.count(), 0)
@@ -51,11 +47,14 @@ class EntryTestCase(TestCase):
         self.assertEquals(self.entry.pingbacks.count(), 0)
         self.assertEquals(self.entry.trackbacks.count(), 0)
 
+        author = User.objects.create_user(username='webmaster',
+                                          email='webmaster@example.com')
+
         comment = Comment.objects.create(comment='My Comment 3',
                                          content_object=self.entry,
                                          site=Site.objects.create(domain='http://toto.com',
                                                                   name='Toto.com'))
-        comment.flags.create(user=self.author, flag=CommentFlag.MODERATOR_APPROVAL)
+        comment.flags.create(user=author, flag=CommentFlag.MODERATOR_APPROVAL)
         self.assertEquals(self.entry.discussions.count(), 2)
         self.assertEquals(self.entry.comments.count(), 2)
         self.assertEquals(self.entry.pingbacks.count(), 0)
@@ -64,7 +63,7 @@ class EntryTestCase(TestCase):
         comment = Comment.objects.create(comment='My Pingback 1',
                                          content_object=self.entry,
                                          site=site)
-        comment.flags.create(user=self.author, flag='pingback')
+        comment.flags.create(user=author, flag='pingback')
         self.assertEquals(self.entry.discussions.count(), 3)
         self.assertEquals(self.entry.comments.count(), 2)
         self.assertEquals(self.entry.pingbacks.count(), 1)
@@ -73,7 +72,7 @@ class EntryTestCase(TestCase):
         comment = Comment.objects.create(comment='My Trackback 1',
                                          content_object=self.entry,
                                          site=site)
-        comment.flags.create(user=self.author, flag='trackback')
+        comment.flags.create(user=author, flag='trackback')
         self.assertEquals(self.entry.discussions.count(), 4)
         self.assertEquals(self.entry.comments.count(), 2)
         self.assertEquals(self.entry.pingbacks.count(), 1)
@@ -101,71 +100,66 @@ class EntryTestCase(TestCase):
         self.entry.start_publication = datetime(2020, 3, 15)
         self.assertFalse(self.entry.is_visible)
 
-    def test_short_url(self):
-        pass
-
     def test_previous_entry(self):
+        site = Site.objects.get_current()
         self.assertFalse(self.entry.previous_entry)
         params = {'title': 'My second entry',
                   'content': 'My second content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-second-entry',
                   'creation_date': datetime(2000, 1, 1),
                   'status': PUBLISHED}
         self.second_entry = Entry.objects.create(**params)
-        self.second_entry.sites.add(Site.objects.get_current())
+        self.second_entry.sites.add(site)
         self.assertEquals(self.entry.previous_entry, self.second_entry)
         params = {'title': 'My third entry',
                   'content': 'My third content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-third-entry',
                   'creation_date': datetime(2001, 1, 1),
                   'status': PUBLISHED}
         self.third_entry = Entry.objects.create(**params)
-        self.third_entry.sites.add(Site.objects.get_current())
+        self.third_entry.sites.add(site)
         self.assertEquals(self.entry.previous_entry, self.third_entry)
         self.assertEquals(self.third_entry.previous_entry, self.second_entry)
 
     def test_next_entry(self):
+        site = Site.objects.get_current()
         self.assertFalse(self.entry.next_entry)
         params = {'title': 'My second entry',
                   'content': 'My second content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-second-entry',
                   'creation_date': datetime(2100, 1, 1),
                   'status': PUBLISHED}
         self.second_entry = Entry.objects.create(**params)
-        self.second_entry.sites.add(Site.objects.get_current())
+        self.second_entry.sites.add(site)
         self.assertEquals(self.entry.next_entry, self.second_entry)
         params = {'title': 'My third entry',
                   'content': 'My third content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-third-entry',
                   'creation_date': datetime(2050, 1, 1),
                   'status': PUBLISHED}
         self.third_entry = Entry.objects.create(**params)
-        self.third_entry.sites.add(Site.objects.get_current())
+        self.third_entry.sites.add(site)
         self.assertEquals(self.entry.next_entry, self.third_entry)
         self.assertEquals(self.third_entry.next_entry, self.second_entry)
 
     def test_related_published_set(self):
+        site = Site.objects.get_current()
         self.assertFalse(self.entry.related_published_set)
         params = {'title': 'My second entry',
                   'content': 'My second content',
-                  'tags': 'zinnia, test',
                   'slug': 'my-second-entry',
                   'status': PUBLISHED}
         self.second_entry = Entry.objects.create(**params)
         self.second_entry.related.add(self.entry)
         self.assertEquals(len(self.entry.related_published_set), 0)
 
-        self.second_entry.sites.add(Site.objects.get_current())
+        self.second_entry.sites.add(site)
         self.assertEquals(len(self.entry.related_published_set), 1)
         self.assertEquals(len(self.second_entry.related_published_set), 0)
 
         self.entry.status = PUBLISHED
         self.entry.save()
-        self.entry.sites.add(Site.objects.get_current())
+        self.entry.sites.add(site)
         self.assertEquals(len(self.entry.related_published_set), 1)
         self.assertEquals(len(self.second_entry.related_published_set), 1)
 
