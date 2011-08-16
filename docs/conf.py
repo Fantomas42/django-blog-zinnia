dc407156d9d402a190ab8ed12848b1f8fec3954d
Fantomas42 <fantomas42@gmail.com>
2011-08-16 15:52:46 +0200

    always build documentation in the build dir

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
