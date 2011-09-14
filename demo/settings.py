9838a0fefb042cfc7c6929c2ed216eab9851cc0b
Fantomas42 <fantomas42@gmail.com>
2011-09-14 16:26:04 +0200

    remove useless template loader

--- demo/settings.py
+++ demo/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'demo.urls'
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
     'django.template.loaders.eggs.load_template_source',
     )
