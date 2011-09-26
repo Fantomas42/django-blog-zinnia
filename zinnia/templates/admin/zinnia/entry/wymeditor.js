a60e423c48135fc7dc9298a5dbeef16fc0ae4e50
Fantomas42 <fantomas42@gmail.com>
2011-09-26 10:34:22 +0200

    replacing ZINNIA_MEDIA_URL by STATIC_URL in the templates

--- zinnia/templates/admin/zinnia/entry/wymeditor.js
+++ zinnia/templates/admin/zinnia/entry/wymeditor.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#id_content").wymeditor({
 	  skin: "django", lang: "{{ LANGUAGE_CODE }}",
-	  stylesheet: "{{ ZINNIA_MEDIA_URL }}css/wymeditor_styles.css",
+	  stylesheet: "{{ STATIC_URL }}zinnia/css/wymeditor_styles.css",
 	  updateSelector: "input:submit", updateEvent: "click",
 	  postInit: function(wym) {
 	      wym.hovertools();
