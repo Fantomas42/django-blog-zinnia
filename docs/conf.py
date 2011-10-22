a0f35ca57e95a91f2b07549b6bda3332271d5301
Fantomas42 <fantomas42@gmail.com>
2011-10-22 18:49:50 +0200

    start using intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,12 @@ import zinnia
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/2.6', None),
+    'django': ('http://readthedocs.org/docs/django/en/latest/', None),
+    'djangocms': ('http://readthedocs.org/docs/django-cms/en/latest/', None),
+    }
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
