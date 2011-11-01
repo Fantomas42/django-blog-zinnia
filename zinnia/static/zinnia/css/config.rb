76c91fd51861e2c3622c186b2a9227fb141c497d
Fantomas42 <fantomas42@gmail.com>
2011-11-01 21:07:54 +0100

    start rewriting css

--- zinnia/static/zinnia/css/config.rb
+++ zinnia/static/zinnia/css/config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
-project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "."
-sass_dir = "src"
+sass_dir = "sass"
 images_dir = "../img"
+javascripts_dir = "../js"
 output_style = :compact
 line_comments = false
