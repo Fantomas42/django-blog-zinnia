89bc263f725923cbe399e0a415e8e07748816e71
Fantomas42 <fantomas42@gmail.com>
2011-11-23 16:07:19 +0100

    remove useless blank lines in templates

--- zinnia/templates/admin/zinnia/entry/autocomplete_tags.js
+++ zinnia/templates/admin/zinnia/entry/autocomplete_tags.js
@@ -1,10 +1,9 @@
 {% load tagging_tags %}
-
 $(document).ready(function() {
   {% tags_for_model zinnia.Entry as entry_tags %}
   var data = "{{ entry_tags|join:',' }}".split(",");
   $("#id_tags").autocomplete(data, {
-                width: 150, max: 10, 
+                width: 150, max: 10,
                 multiple: true, multipleSeparator: ", ",
                 scroll: true, scrollHeight: 300,
                 matchContains: true, autoFill: true,});
