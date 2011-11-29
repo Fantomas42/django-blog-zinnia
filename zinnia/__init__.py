16ad842f0fac21ea15092ab3dba16c27627abb4d
Fantomas42 <fantomas42@gmail.com>
2011-11-29 17:40:01 +0100

    passing zinnia.__url__ to https

--- zinnia/__init__.py
+++ zinnia/__init__.py
@@ -5,4 +5,4 @@ __license__ = 'BSD License'
 __author__ = 'Fantomas42'
 __email__ = 'fantomas42@gmail.com'
 
-__url__ = 'http://github.com/Fantomas42/django-blog-zinnia'
+__url__ = 'https://github.com/Fantomas42/django-blog-zinnia'
