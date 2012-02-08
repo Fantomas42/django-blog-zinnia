7d5c7d684cb25cc072ba0d38134cb7bed42664e1
Fantomas42 <fantomas42@gmail.com>
2012-02-08 15:40:06 +0100

    entry_search view is now generic

--- zinnia/views/search.py
+++ zinnia/views/search.py
@@ -6,11 +6,12 @@ from zinnia.models import Entry
 from zinnia.settings import PAGINATION
 
 
-def entry_search(request):
+def entry_search(request, **kwargs):
     """Search entries matching with a pattern"""
     error = None
     pattern = None
     entries = Entry.published.none()
+    extra_context = kwargs.pop('extra_context', {})
 
     if request.GET:
         pattern = request.GET.get('pattern', '')
@@ -21,8 +22,11 @@ def entry_search(request):
     else:
         error = _('No pattern to search found')
 
+    if not kwargs.get('template_name'):
+        kwargs['template_name'] = 'zinnia/entry_search.html'
+
+    extra_context.update({'error': error, 'pattern': pattern})
+    kwargs['extra_context'] = extra_context
+
     return object_list(request, queryset=entries,
-                       paginate_by=PAGINATION,
-                       template_name='zinnia/entry_search.html',
-                       extra_context={'error': error,
-                                      'pattern': pattern})
+                       paginate_by=PAGINATION, **kwargs)
