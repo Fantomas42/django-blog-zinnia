f6a527d2c9860e1cc165e7e21423827d2cda380c
Fantomas42 <fantomas42@gmail.com>
2012-02-08 15:56:32 +0100

    changing prototype of entry_channel view

--- zinnia/views/channels.py
+++ zinnia/views/channels.py
@@ -4,8 +4,9 @@ from django.views.generic.list_detail import object_list
 from zinnia.models import Entry
 
 
-def entry_channel(request, query, *ka, **kw):
-    """Display a custom selection of entries"""
+def entry_channel(request, query, **kwargs):
+    """View for displaying a custom selection of entries
+    based on a search pattern, useful for SEO/SMO pages"""
     queryset = Entry.published.search(query)
     return object_list(request, queryset=queryset,
-                       *ka, **kw)
+                       **kwargs)
