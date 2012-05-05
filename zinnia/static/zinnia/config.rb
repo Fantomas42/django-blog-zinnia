e9a10eeaa999fb99c11a906d1db713092d695e41
Fantomas42 <fantomas42@gmail.com>
2012-05-05 19:31:12 +0200

    review position of sass-dir and use url helper for image location

---
+++ zinnia/static/zinnia/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+require 'susy'
+# Project directories
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+# CSS generation
+output_style = :compressed
+line_comments = false
+relative_assets = true
