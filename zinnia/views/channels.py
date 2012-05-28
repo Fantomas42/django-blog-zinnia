b783efa74ba8887d9efb45a92fa1e2e784af33c5
Fantomas42 <fantomas42@gmail.com>
2012-05-28 16:26:16 +0200

    adding pagination to channels

--- zinnia/views/channels.py
+++ zinnia/views/channels.py
@@ -2,12 +2,14 @@
 from django.views.generic.list import ListView
 
 from zinnia.models import Entry
+from zinnia.settings import PAGINATION
 
 
 class EntryChannel(ListView):
     """View for displaying a custom selection of entries
     based on a search pattern, useful for SEO/SMO pages"""
     query = ''
+    paginate_by = PAGINATION
 
     def get_queryset(self):
         """Override the get_queryset method to do the search"""
