7d43c4904738c0625ea4d4e18b6eacd47e11675c
Fantomas42 <fantomas42@gmail.com>
2012-10-20 17:09:18 +0200

    fix import of the models at the loading, close #158, close #161, close #162, close #164, close #172

--- zinnia/models/__init__.py
+++ zinnia/models/__init__.py
@@ -1,1 +1,12 @@
 """Models for Zinnia"""
+# Here we import the Zinnia's Model classes
+# to register the Models at the loading, not
+# when the Zinnia's urls are parsed. Issue #161.
+from zinnia.models.entry import Entry
+from zinnia.models.author import Author
+from zinnia.models.category import Category
+
+
+__all__ = [Entry.__name__,
+           Author.__name__,
+           Category.__name__]
