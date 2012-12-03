df007861ba9048760344cf46becc934f2c8c56fd
Fantomas42 <fantomas42@gmail.com>
2012-12-03 16:11:25 +0100

    Prefetch categories and authors for archive views

--- zinnia/views/archives.py
+++ zinnia/views/archives.py
@@ -12,6 +12,7 @@ from zinnia.models.entry import Entry
 from zinnia.views.mixins.archives import ArchiveMixin
 from zinnia.views.mixins.archives import PreviousNextPublishedMixin
 from zinnia.views.mixins.callable_queryset import CallableQuerysetMixin
+from zinnia.views.mixins.prefetch_related import PrefetchCategoriesAuthorsMixin
 from zinnia.views.mixins.templates import \
      EntryQuerysetArchiveTemplateResponseMixin
 from zinnia.views.mixins.templates import \
@@ -23,16 +24,18 @@ from zinnia.views.mixins.tz_fixes import EntryMonthTZFix
 
 class EntryArchiveMixin(ArchiveMixin,
                         PreviousNextPublishedMixin,
+                        PrefetchCategoriesAuthorsMixin,
                         CallableQuerysetMixin,
                         EntryQuerysetArchiveTemplateResponseMixin):
     """
     Mixin combinating:
 
-    - ArchiveMixin configuration centralizing conf for archive views
-    - PreviousNextPublishedMixin for returning published archives
-    - CallableQueryMixin to force the update of the queryset
+    - ArchiveMixin configuration centralizing conf for archive views.
+    - PrefetchCategoriesAuthorsMixin to prefetch related objects.
+    - PreviousNextPublishedMixin for returning published archives.
+    - CallableQueryMixin to force the update of the queryset.
     - EntryQuerysetArchiveTemplateResponseMixin to provide a
-      custom templates for archives
+      custom templates for archives.
     """
     queryset = Entry.published.all
 
@@ -40,28 +43,38 @@ class EntryArchiveMixin(ArchiveMixin,
 class EntryIndex(EntryArchiveMixin,
                  EntryQuerysetArchiveTodayTemplateResponseMixin,
                  BaseArchiveIndexView):
-    """View returning the archive index"""
+    """
+    View returning the archive index.
+    """
     context_object_name = 'entry_list'
 
 
 class EntryYear(EntryArchiveMixin, BaseYearArchiveView):
-    """View returning the archive for a year"""
+    """
+    View returning the archives for a year.
+    """
     make_object_list = True
     template_name_suffix = '_archive_year'
 
 
 class EntryMonth(EntryMonthTZFix, EntryArchiveMixin, BaseMonthArchiveView):
-    """View returning the archive for a month"""
+    """
+    View returning the archives for a month.
+    """
     template_name_suffix = '_archive_month'
 
 
 class EntryWeek(EntryWeekTZFix, EntryArchiveMixin, BaseWeekArchiveView):
-    """View returning the archive for a week"""
+    """
+    View returning the archive for a week.
+    """
     template_name_suffix = '_archive_week'
 
     def get_dated_items(self):
-        """Override get_dated_items to add a useful 'week_end_day'
-        variable in the extra context of the view"""
+        """
+        Override get_dated_items to add a useful 'week_end_day'
+        variable in the extra context of the view.
+        """
         self.date_list, self.object_list, extra_context = super(
             EntryWeek, self).get_dated_items()
         extra_context['week_end_day'] = extra_context[
@@ -70,18 +83,24 @@ class EntryWeek(EntryWeekTZFix, EntryArchiveMixin, BaseWeekArchiveView):
 
 
 class EntryDay(EntryDayTZFix, EntryArchiveMixin, BaseDayArchiveView):
-    """View returning the archive for a day"""
+    """
+    View returning the archive for a day.
+    """
     template_name_suffix = '_archive_day'
 
 
 class EntryToday(EntryDayTZFix, EntryArchiveMixin, BaseTodayArchiveView):
-    """View returning the archive for the current day"""
+    """
+    View returning the archive for the current day.
+    """
     template_name_suffix = '_archive_today'
 
     def get_dated_items(self):
-        """Return (date_list, items, extra_context) for this request.
+        """
+        Return (date_list, items, extra_context) for this request.
         And defines self.year/month/day for
-        EntryQuerysetArchiveTemplateResponseMixin."""
+        EntryQuerysetArchiveTemplateResponseMixin.
+        """
         today = datetime.date.today()
         self.year, self.month, self.day = today.isoformat().split('-')
         return self._get_dated_items(today)
