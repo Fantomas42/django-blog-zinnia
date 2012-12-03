bfea199a4c284f4e1bd2bba174a995b0a42164cc
Fantomas42 <fantomas42@gmail.com>
2012-12-03 16:11:49 +0100

    Prefetch categories and authors for category detail view

--- zinnia/views/categories.py
+++ zinnia/views/categories.py
@@ -6,6 +6,7 @@ from django.views.generic.list import BaseListView
 from zinnia.models.category import Category
 from zinnia.settings import PAGINATION
 from zinnia.views.mixins.templates import EntryQuerysetTemplateResponseMixin
+from zinnia.views.mixins.prefetch_related import PrefetchCategoriesAuthorsMixin
 
 
 def get_category_or_404(path):
@@ -15,28 +16,53 @@ def get_category_or_404(path):
 
 
 class CategoryList(ListView):
-    """View returning a list of all the categories"""
+    """
+    View returning a list of all the categories.
+    """
     queryset = Category.objects.all()
 
 
-class CategoryDetail(EntryQuerysetTemplateResponseMixin, BaseListView):
-    """View returning a list of all the entries
-    belonging to a category"""
+class BaseCategoryDetail(object):
+    """
+    Mixin providing the behavior of the category detail view,
+    by returning in the context the current category and a
+    queryset containing the entries belonging into.
+    """
+
+    def get_queryset(self):
+        """
+        Retrieve the category by his path and
+        build a queryset of her published entries.
+        """
+        self.category = get_category_or_404(self.kwargs['path'])
+        return self.category.entries_published()
+
+    def get_context_data(self, **kwargs):
+        """
+        Add the current category in context
+        """
+        context = super(BaseCategoryDetail, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
+class CategoryDetail(EntryQuerysetTemplateResponseMixin,
+                     PrefetchCategoriesAuthorsMixin,
+                     BaseCategoryDetail,
+                     BaseListView):
+    """
+    Detailed view for a Category combinating these mixins:
+
+    - EntryQuerysetTemplateResponseMixin to provide custom templates
+      for the category display.
+    - PrefetchCategoriesAuthorsMixin to prefetch related Categories
+      and Authors to belonging the entry list.
+    - BaseCategoryDetail to provide the behavior of the view.
+    - BaseListView to implement the ListView.
+    """
     model_type = 'category'
     paginate_by = PAGINATION
 
     def get_model_name(self):
         """The model name is the category's slug"""
         return self.category.slug
-
-    def get_queryset(self):
-        """Return a queryset of entries published
-        belonging to the current category"""
-        self.category = get_category_or_404(self.kwargs['path'])
-        return self.category.entries_published()
-
-    def get_context_data(self, **kwargs):
-        """Add the current category in context"""
-        context = super(CategoryDetail, self).get_context_data(**kwargs)
-        context['category'] = self.category
-        return context
