434ce20bbdbad7b650576190d5e788a01dcf7ec2
Fantomas42 <fantomas42@gmail.com>
2012-12-03 18:11:35 +0100

    Prefetch categories and authors for channel entry view

--- zinnia/views/channels.py
+++ zinnia/views/channels.py
@@ -3,14 +3,43 @@ from django.views.generic.list import ListView
 
 from zinnia.models.entry import Entry
 from zinnia.settings import PAGINATION
+from zinnia.views.mixins.prefetch_related import PrefetchCategoriesAuthorsMixin
 
 
-class EntryChannel(ListView):
-    """View for displaying a custom selection of entries
-    based on a search pattern, useful for SEO/SMO pages"""
+class BaseEntryChannel(object):
+    """
+    Mixin for displaying a custom selection of entries
+    based on a search query, useful to build SEO/SMO pages
+    aggregating entries on a thematic or for building a
+    custom homepage.
+    """
     query = ''
-    paginate_by = PAGINATION
 
     def get_queryset(self):
-        """Override the get_queryset method to do the search"""
+        """
+        Override the get_queryset method to build
+        the queryset with entry matching query.
+        """
         return Entry.published.search(self.query)
+
+    def get_context_data(self, **kwargs):
+        """
+        Add query in context.
+        """
+        context = super(BaseEntryChannel, self).get_context_data(**kwargs)
+        context.update({'query': self.query})
+        return context
+
+
+class EntryChannel(PrefetchCategoriesAuthorsMixin,
+                   BaseEntryChannel,
+                   ListView):
+    """
+    Channel view for entries combinating these mixins:
+
+    - PrefetchCategoriesAuthorsMixin to prefetch related Categories
+      and Authors to belonging the entry list.
+    - BaseEntryChannel to provide the behavior of the view.
+    - ListView to implement the ListView and template name resolution.
+    """
+    paginate_by = PAGINATION
