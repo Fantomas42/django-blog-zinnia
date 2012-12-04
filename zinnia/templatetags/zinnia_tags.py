953639f7bea2b859da24591ff991620922df3163
Fantomas42 <fantomas42@gmail.com>
2012-12-04 11:01:23 +0100

    use prefetch_related for recent_comments and recent_linkback instead of cache_content_object_entries

--- zinnia/templatetags/zinnia_tags.py
+++ zinnia/templatetags/zinnia_tags.py
@@ -205,19 +205,6 @@ def get_calendar_entries(context, year=None, month=None,
                 next_month=next_month)}
 
 
-def cache_content_object_entries(queryset):
-    """
-    Cache the generic relation field of all
-    the comments in the queryset.
-    """
-    entries_related = set()
-    for item in queryset:
-        entries_related.add(item.object_pk)
-    entries = Entry.objects.in_bulk(list(entries_related))
-    for item in queryset:
-        setattr(item, '_content_object_cache', entries[int(item.object_pk)])
-
-
 @register.inclusion_tag('zinnia/tags/dummy.html')
 def get_recent_comments(number=5, template='zinnia/tags/recent_comments.html'):
     """Return the most recent comments"""
@@ -231,7 +218,7 @@ def get_recent_comments(number=5, template='zinnia/tags/recent_comments.html'):
         content_type=content_type, object_pk__in=entry_published_pks,
         is_public=True).order_by('-submit_date')[:number]
 
-    cache_content_object_entries(comments)
+    comments = comments.prefetch_related('content_object')
 
     return {'template': template,
             'comments': comments}
@@ -252,7 +239,7 @@ def get_recent_linkbacks(number=5,
         is_public=True).order_by(
         '-submit_date')[:number]
 
-    cache_content_object_entries(linkbacks)
+    linkbacks = linkbacks.prefetch_related('content_object')
 
     return {'template': template,
             'linkbacks': linkbacks}
