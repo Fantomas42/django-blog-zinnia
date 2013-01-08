1124cac7b6a484bbf0cb7dd0eefff0c70b3b7f34
Fantomas42 <fantomas42@gmail.com>
2013-01-08 17:10:35 +0100

    split EntryAbstractClass into different model mixin for being able to override a field easily

--- zinnia/models/entry.py
+++ zinnia/models/entry.py
@@ -35,78 +35,58 @@ from zinnia.managers import DRAFT, HIDDEN, PUBLISHED
 from zinnia.url_shortener import get_url_shortener
 
 
-class EntryAbstractClass(models.Model):
-    """Base Model design for publishing entries"""
+class CoreEntry(models.Model):
+    """
+    Abstract core entry model class providing
+    the fields and methods required for publishing
+    content over time.
+    """
     STATUS_CHOICES = ((DRAFT, _('draft')),
                       (HIDDEN, _('hidden')),
                       (PUBLISHED, _('published')))
 
-    title = models.CharField(_('title'), max_length=255)
+    title = models.CharField(
+        _('title'), max_length=255)
 
-    image = models.ImageField(_('image'), upload_to=UPLOAD_TO,
-                              blank=True, help_text=_('used for illustration'))
     content = models.TextField(_('content'))
-    excerpt = models.TextField(_('excerpt'), blank=True,
-                               help_text=_('optional element'))
 
-    tags = TagField(_('tags'))
-    categories = models.ManyToManyField(Category, verbose_name=_('categories'),
-                                        related_name='entries',
-                                        blank=True, null=True)
-    related = models.ManyToManyField('self', verbose_name=_('related entries'),
-                                     blank=True, null=True)
+    slug = models.SlugField(
+        _('slug'), max_length=255,
+        unique_for_date='creation_date',
+        help_text=_("Used to build the entry's URL."))
 
-    slug = models.SlugField(help_text=_("used to build the entry's URL"),
-                            unique_for_date='creation_date',
-                            max_length=255)
+    status = models.IntegerField(
+        _('status'), choices=STATUS_CHOICES, default=DRAFT)
 
-    authors = models.ManyToManyField(Author, verbose_name=_('authors'),
-                                     related_name='entries',
-                                     blank=True, null=False)
+    start_publication = models.DateTimeField(
+        _('start publication'), blank=True, null=True,
+        help_text=_('Start date of publication.'))
 
-    status = models.IntegerField(choices=STATUS_CHOICES, default=DRAFT)
-    featured = models.BooleanField(_('featured'), default=False)
-    comment_enabled = models.BooleanField(_('comment enabled'), default=True)
-    pingback_enabled = models.BooleanField(_('linkback enabled'), default=True)
+    end_publication = models.DateTimeField(
+        _('end publication'), blank=True, null=True,
+        help_text=_('End date of publication.'))
+
+    sites = models.ManyToManyField(
+        Site,
+        related_name='entries',
+        verbose_name=_('sites'),
+        help_text=_('Sites where the entry will be published.'))
 
     creation_date = models.DateTimeField(
         _('creation date'), default=timezone.now,
-        help_text=_("used to build the entry's URL"))
-    last_update = models.DateTimeField(_('last update'), default=timezone.now)
-    start_publication = models.DateTimeField(_('start publication'),
-                                             blank=True, null=True,
-                                             help_text=_('date start publish'))
-    end_publication = models.DateTimeField(_('end publication'),
-                                           blank=True, null=True,
-                                           help_text=_('date end publish'))
+        help_text=_("Used to build the entry's URL."))
 
-    sites = models.ManyToManyField(Site, verbose_name=_('sites publication'),
-                                   related_name='entries')
-
-    login_required = models.BooleanField(
-        _('login required'), default=False,
-        help_text=_('only authenticated users can view the entry'))
-    password = models.CharField(
-        _('password'), max_length=50, blank=True,
-        help_text=_('protect the entry with a password'))
-
-    template = models.CharField(
-        _('template'), max_length=250,
-        default='entry_detail.html',
-        choices=[('entry_detail.html', _('Default template'))] +
-        ENTRY_TEMPLATES,
-        help_text=_('template used to display the entry'))
-
-    comment_count = models.IntegerField(_('comment count'), default=0)
-    pingback_count = models.IntegerField(_('pingback count'), default=0)
-    trackback_count = models.IntegerField(_('trackback count'), default=0)
+    last_update = models.DateTimeField(
+        _('last update'), default=timezone.now)
 
     objects = models.Manager()
     published = EntryPublishedManager()
 
     @property
     def html_content(self):
-        """Return the Entry.content attribute formatted in HTML"""
+        """
+        Returns the "content" field formatted in HTML.
+        """
         if MARKUP_LANGUAGE == 'markdown':
             return markdown(self.content, MARKDOWN_EXTENSIONS)
         elif MARKUP_LANGUAGE == 'textile':
@@ -117,30 +97,18 @@ class EntryAbstractClass(models.Model):
             return linebreaks(self.content)
         return self.content
 
-    @cached_property
-    def previous_entry(self):
-        """Return the previous entry"""
-        entries = Entry.published.filter(
-            creation_date__lt=self.creation_date)[:1]
-        if entries:
-            return entries[0]
-
-    @cached_property
-    def next_entry(self):
-        """Return the next entry"""
-        entries = Entry.published.filter(
-            creation_date__gt=self.creation_date).order_by('creation_date')[:1]
-        if entries:
-            return entries[0]
-
     @property
     def word_count(self):
-        """Count the words of an entry"""
+        """
+        Counts the number of words used in an entry.
+        """
         return len(strip_tags(self.html_content).split())
 
     @property
     def is_actual(self):
-        """Check if an entry is within publication period"""
+        """
+        Checks if an entry is within his publication period.
+        """
         now = timezone.now()
         if self.start_publication and now < self.start_publication:
             return False
@@ -151,61 +119,44 @@ class EntryAbstractClass(models.Model):
 
     @property
     def is_visible(self):
-        """Check if an entry is visible on site"""
+        """
+        Checks if an entry is visible and published.
+        """
         return self.is_actual and self.status == PUBLISHED
 
-    @property
-    def related_published(self):
-        """Return only related entries published"""
-        return entries_published(self.related)
+    @cached_property
+    def previous_entry(self):
+        """
+        Returns the previous published entry if exists.
+        """
+        entries = Entry.published.filter(
+            creation_date__lt=self.creation_date)[:1]
+        if entries:
+            return entries[0]
 
-    @property
-    def tags_list(self):
-        """Return iterable list of tags"""
-        return parse_tag_input(self.tags)
-
-    @property
-    def discussions(self):
-        """Return queryset of published discussions"""
-        return comments.get_model().objects.for_model(
-            self).filter(is_public=True, is_removed=False)
-
-    @property
-    def comments(self):
-        """Return queryset of published comments"""
-        return self.discussions.filter(Q(flags=None) | Q(
-            flags__flag=CommentFlag.MODERATOR_APPROVAL))
-
-    @property
-    def pingbacks(self):
-        """Return queryset of published pingbacks"""
-        return self.discussions.filter(flags__flag=PINGBACK)
-
-    @property
-    def trackbacks(self):
-        """Return queryset of published trackbacks"""
-        return self.discussions.filter(flags__flag=TRACKBACK)
-
-    @property
-    def comments_are_open(self):
-        """Check if comments are open"""
-        if AUTO_CLOSE_COMMENTS_AFTER and self.comment_enabled:
-            return (timezone.now() - (
-                self.start_publication or self.creation_date)).days < \
-                AUTO_CLOSE_COMMENTS_AFTER
-        return self.comment_enabled
+    @cached_property
+    def next_entry(self):
+        """
+        Returns the next published entry if exists.
+        """
+        entries = Entry.published.filter(
+            creation_date__gt=self.creation_date).order_by('creation_date')[:1]
+        if entries:
+            return entries[0]
 
     @property
     def short_url(self):
-        """Return the entry's short url"""
+        """
+        Returns the entry's short url.
+        """
         return get_url_shortener()(self)
-
-    def __unicode__(self):
-        return u'%s: %s' % (self.title, self.get_status_display())
 
     @models.permalink
     def get_absolute_url(self):
-        """Return entry's URL"""
+        """
+        Builds and returns the entry's URL based on
+        the slug and the creation date.
+        """
         creation_date = timezone.localtime(self.creation_date)
         return ('zinnia_entry_detail', (), {
             'year': creation_date.strftime('%Y'),
@@ -213,8 +164,13 @@ class EntryAbstractClass(models.Model):
             'day': creation_date.strftime('%d'),
             'slug': self.slug})
 
+    def __unicode__(self):
+        return u'%s: %s' % (self.title, self.get_status_display())
+
     class Meta:
-        """Entry's Meta"""
+        """
+        CoreEntry's meta informations.
+        """
         abstract = True
         app_label = 'zinnia'
         ordering = ['-creation_date']
@@ -226,9 +182,246 @@ class EntryAbstractClass(models.Model):
                        ('can_change_author', 'Can change author(s)'), )
 
 
+class DiscussionsEntry(models.Model):
+    """
+    Abstract discussion model class providing
+    the fields and methods to manage the discussions
+    (comments, pingbacks, trackbacks).
+    """
+    comment_enabled = models.BooleanField(
+        _('comments enabled'), default=True,
+        help_text=_('Allows comments if checked.'))
+    pingback_enabled = models.BooleanField(
+        _('linkback enabled'), default=True,
+        help_text=_('Allows linkbacks if checked.'))
+
+    comment_count = models.IntegerField(
+        _('comment count'), default=0)
+    pingback_count = models.IntegerField(
+        _('pingback count'), default=0)
+    trackback_count = models.IntegerField(
+        _('trackback count'), default=0)
+
+    @property
+    def discussions(self):
+        """
+        Returns a queryset of the published discussions.
+        """
+        return comments.get_model().objects.for_model(
+            self).filter(is_public=True, is_removed=False)
+
+    @property
+    def comments(self):
+        """
+        Returns a queryset of the published comments.
+        """
+        return self.discussions.filter(Q(flags=None) | Q(
+            flags__flag=CommentFlag.MODERATOR_APPROVAL))
+
+    @property
+    def pingbacks(self):
+        """
+        Returns a queryset of the published pingbacks.
+        """
+        return self.discussions.filter(flags__flag=PINGBACK)
+
+    @property
+    def trackbacks(self):
+        """
+        Return a queryset of the published trackbacks.
+        """
+        return self.discussions.filter(flags__flag=TRACKBACK)
+
+    @property
+    def comments_are_open(self):
+        """
+        Checks if the comments are open with the
+        AUTO_CLOSE_COMMENTS_AFTER setting.
+        """
+        if AUTO_CLOSE_COMMENTS_AFTER and self.comment_enabled:
+            return (timezone.now() - (
+                self.start_publication or self.creation_date)).days < \
+                AUTO_CLOSE_COMMENTS_AFTER
+        return self.comment_enabled
+
+    class Meta:
+        abstract = True
+
+
+class RelatedEntry(models.Model):
+    """
+    Abstract model class for making manual relations
+    between the differents entries.
+    """
+    related = models.ManyToManyField(
+        'self',
+        blank=True, null=True,
+        verbose_name=_('related entries'))
+
+    @property
+    def related_published(self):
+        """
+        Returns only related entries published.
+        """
+        return entries_published(self.related)
+
+    class Meta:
+        abstract = True
+
+
+class ExcerptEntry(models.Model):
+    """
+    Abstract model class to add an excerpt to the entries.
+    """
+    excerpt = models.TextField(
+        _('excerpt'), blank=True,
+        help_text=_('Optional element.'))
+
+    class Meta:
+        abstract = True
+
+
+class ImageEntry(models.Model):
+    """
+    Abstract model class to add an image to the entries.
+    """
+    image = models.ImageField(
+        _('image'), blank=True, upload_to=UPLOAD_TO,
+        help_text=_('Used for illustration.'))
+
+    class Meta:
+        abstract = True
+
+
+class FeaturedEntry(models.Model):
+    """
+    Abstract model class to mark entries as featured.
+    """
+    featured = models.BooleanField(
+        _('featured'), default=False)
+
+    class Meta:
+        abstract = True
+
+
+class AuthorsEntry(models.Model):
+    """
+    Abstract model class to add relationship
+    between the entries and their authors.
+    """
+    authors = models.ManyToManyField(
+        Author,
+        related_name='entries',
+        blank=True, null=False,
+        verbose_name=_('authors'))
+
+    class Meta:
+        abstract = True
+
+
+class CategoriesEntry(models.Model):
+    """
+    Abstract model class to categorize the entries.
+    """
+    categories = models.ManyToManyField(
+        Category,
+        related_name='entries',
+        blank=True, null=True,
+        verbose_name=_('categories'))
+
+    class Meta:
+        abstract = True
+
+
+class TagsEntry(models.Model):
+    """
+    Abstract lodel class to add tags to the entries.
+    """
+    tags = TagField(_('tags'))
+
+    @property
+    def tags_list(self):
+        """
+        Return iterable list of tags.
+        """
+        return parse_tag_input(self.tags)
+
+    class Meta:
+        abstract = True
+
+
+class LoginRequiredEntry(models.Model):
+    """
+    Abstract model class to restrcit the display
+    of the entry on authenticated users.
+    """
+    login_required = models.BooleanField(
+        _('login required'), default=False,
+        help_text=_('Only authenticated users can view the entry.'))
+
+    class Meta:
+        abstract = True
+
+
+class PasswordRequiredEntry(models.Model):
+    """
+    Abstract model class to restrict the display
+    of the entry to users knowing the password.
+    """
+    password = models.CharField(
+        _('password'), max_length=50, blank=True,
+        help_text=_('Protects the entry with a password.'))
+
+    class Meta:
+        abstract = True
+
+
+class DetailTemplateEntry(models.Model):
+    """
+    Abstract model class to display entries with a
+    custom template if needed on the detail page.
+    """
+    template = models.CharField(
+        _('template'), max_length=250,
+        default='entry_detail.html',
+        choices=[('entry_detail.html', _('Default template'))] +
+        ENTRY_TEMPLATES,
+        help_text=_("Template used to display the entry's detail page."))
+
+    class Meta:
+        abstract = True
+
+
+class EntryAbstractClass(
+        CoreEntry,
+        DiscussionsEntry,
+        RelatedEntry,
+        ExcerptEntry,
+        ImageEntry,
+        FeaturedEntry,
+        AuthorsEntry,
+        CategoriesEntry,
+        TagsEntry,
+        LoginRequiredEntry,
+        PasswordRequiredEntry,
+        DetailTemplateEntry):
+    """
+    Final abstract entry model class assembling
+    all the abstract entry model classes into a single one.
+
+    In this manner we can override some fields without
+    reimplemting all the EntryAbstractClass.
+    """
+
+    class Meta(CoreEntry.Meta):
+        abstract = True
+
+
 def get_base_model():
-    """Determine the base Model to inherit in the
-    Entry Model, this allow to overload it."""
+    """
+    Determine the base Model to inherit in the
+    Entry Model, this allow to overload it.
+    """
     if not ENTRY_BASE_MODEL:
         return EntryAbstractClass
 
@@ -247,6 +440,7 @@ def get_base_model():
 class Entry(get_base_model()):
     """
     The final Entry model based on inheritence.
+
     Check this out for customizing the Entry Model class:
     http://django-blog-zinnia.rtfd.org/extending-entry
     """
