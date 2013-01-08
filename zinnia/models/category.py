66438555b2d5a85f3b1fea699001d58ca0e0eaaf
Fantomas42 <fantomas42@gmail.com>
2013-01-08 17:09:21 +0100

    review Category model

--- zinnia/models/category.py
+++ zinnia/models/category.py
@@ -11,48 +11,69 @@ from zinnia.managers import EntryRelatedPublishedManager
 
 
 class Category(MPTTModel):
-    """Category model for Entry"""
+    """
+    Simple model for categorizing entries.
+    """
 
-    title = models.CharField(_('title'), max_length=255)
-    slug = models.SlugField(help_text=_('used for publication'),
-                            unique=True, max_length=255)
-    description = models.TextField(_('description'), blank=True)
+    title = models.CharField(
+        _('title'), max_length=255)
 
-    parent = TreeForeignKey('self', null=True, blank=True,
-                            verbose_name=_('parent category'),
-                            related_name='children')
+    slug = models.SlugField(
+        _('slug'), unique=True, max_length=255,
+        help_text=_("Used to build the category's URL."))
+
+    description = models.TextField(
+        _('description'), blank=True)
+
+    parent = TreeForeignKey(
+        'self',
+        related_name='children',
+        null=True, blank=True,
+        verbose_name=_('parent category'))
 
     objects = TreeManager()
     published = EntryRelatedPublishedManager()
 
     def entries_published(self):
-        """Return only the entries published"""
+        """
+        Returns category's published entries.
+        """
         return entries_published(self.entries)
 
     @property
     def tree_path(self):
-        """Return category's tree path, by his ancestors"""
+        """
+        Returns category's tree path
+        by concatening the slug of his ancestors.
+        """
         if self.parent_id:
             return '/'.join(
                 [ancestor.slug for ancestor in self.get_ancestors()] +
                 [self.slug])
         return self.slug
 
+    @models.permalink
+    def get_absolute_url(self):
+        """
+        Builds and returns the category's URL
+        based on his tree path.
+        """
+        return ('zinnia_category_detail', (self.tree_path,))
+
     def __unicode__(self):
         return self.title
 
-    @models.permalink
-    def get_absolute_url(self):
-        """Return category's URL"""
-        return ('zinnia_category_detail', (self.tree_path,))
-
     class Meta:
-        """Category's Meta"""
+        """
+        Category's meta informations.
+        """
         app_label = 'zinnia'
         ordering = ['title']
         verbose_name = _('category')
         verbose_name_plural = _('categories')
 
     class MPTTMeta:
-        """Category MPTT's Meta"""
+        """
+        Category MPTT's meta informations.
+        """
         order_insertion_by = ['title']
