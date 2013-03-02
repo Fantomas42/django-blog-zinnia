fd479e424ee7f7359dd16bb5d4087c981db2a82a
Fantomas42 <fantomas42@gmail.com>
2013-03-02 17:23:14 +0100

    remove "{% load url from future %}"

--- zinnia/templates/admin/zinnia/entry/markitup.js
+++ zinnia/templates/admin/zinnia/entry/markitup.js
@@ -1,4 +1,3 @@
-{% load url from future %}
 $(document).ready(function() {
   mySettings["previewParserPath"] = "{% url 'admin:zinnia_entry_markitup_preview' %}";
   $("#id_content").markItUp(mySettings);
