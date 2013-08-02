1f0dfd7de3a9bb0dea0ede7db8c926f85a82fd3c
Fantomas42 <fantomas42@gmail.com>
2013-08-02 11:22:17 +0200

    Remove useless tests/templates

--- zinnia/testsettings.py
+++ zinnia/testsettings.py
@@ -32,9 +32,6 @@ TEMPLATE_LOADERS = [
      ]
 ]
 
-TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__),
-                              'tests', 'templates')]
-
 INSTALLED_APPS = ['django.contrib.contenttypes',
                   'django.contrib.staticfiles',
                   'django.contrib.comments',
