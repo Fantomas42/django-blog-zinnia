78915664179c4c2b3fc974fcf54cfe253689c154
Fantomas42 <fantomas42@gmail.com>
2013-12-30 14:39:21 +0100

    Remove disconnection of the signals when loading the zinnia.tests modules for compatibility

--- zinnia/tests/__init__.py
+++ zinnia/tests/__init__.py
@@ -1,6 +1,1 @@
 """Unit tests for Zinnia"""
-from zinnia.signals import disconnect_entry_signals
-from zinnia.signals import disconnect_discussion_signals
-
-disconnect_entry_signals()
-disconnect_discussion_signals()
