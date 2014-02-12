8816a7ad469599369cbd71b88301bbd5a903f124
Fantomas42 <fantomas42@gmail.com>
2014-02-12 12:30:46 +0100

    Handle error 400 in debug

--- demo/urls.py
+++ demo/urls.py
@@ -41,6 +41,7 @@ urlpatterns += patterns(
 
 urlpatterns += patterns(
     '',
+    url(r'^400/$', 'django.views.defaults.bad_request'),
     url(r'^403/$', 'django.views.defaults.permission_denied'),
     url(r'^404/$', 'django.views.defaults.page_not_found'),
     url(r'^500/$', 'django.views.defaults.server_error'),
