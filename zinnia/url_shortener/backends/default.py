d02036f11cf890e6fdf075a606fc6900da7a3532
Fantomas42 <fantomas42@gmail.com>
2014-04-01 15:36:13 +0200

    Encode the PK under the base 36

--- zinnia/url_shortener/backends/default.py
+++ zinnia/url_shortener/backends/default.py
@@ -1,13 +1,22 @@
 """Default URL shortener backend for Zinnia"""
+import math
+import string
+
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 
 from zinnia.settings import PROTOCOL
+
+base36 = lambda x: ''.join(
+    [(string.digits + string.lowercase)[(x / 36 ** i) % 36]
+     for i in xrange(int(math.log(x, 36)), -1, -1)]
+    )
 
 
 def backend(entry):
     """
     Default URL shortener backend for Zinnia.
     """
-    return '%s://%s%s' % (PROTOCOL, Site.objects.get_current().domain,
-                          reverse('zinnia:entry_shortlink', args=[entry.pk]))
+    return '%s://%s%s' % (
+        PROTOCOL, Site.objects.get_current().domain,
+        reverse('zinnia:entry_shortlink', args=[base36(entry.pk)]))
