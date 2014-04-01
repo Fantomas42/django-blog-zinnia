da1ca4f6ff418baf11e8754d81d9bc32e000f4be
Fantomas42 <fantomas42@gmail.com>
2014-04-01 15:41:17 +0200

    Handle base36 token

--- zinnia/urls/shortlink.py
+++ zinnia/urls/shortlink.py
@@ -7,7 +7,7 @@ from zinnia.views.shortlink import EntryShortLink
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<pk>\d+)/$',
+    url(r'^(?P<token>[\da-z]+)/$',
         EntryShortLink.as_view(),
         name='entry_shortlink'),
 )
