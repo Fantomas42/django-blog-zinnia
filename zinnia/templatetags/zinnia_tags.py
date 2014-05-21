29d23b2cdee74545349abbf13573f48bead11bda
Fantomas42 <fantomas42@gmail.com>
2014-05-21 17:38:15 +0200

    Templatetags now use django_comments

--- zinnia/templatetags/zinnia_tags.py
+++ zinnia/templatetags/zinnia_tags.py
@@ -18,9 +18,10 @@ from django.utils.translation import ugettext as _
 from django.utils.html import conditional_escape
 from django.template.defaultfilters import stringfilter
 from django.contrib.auth import get_user_model
-from django.contrib.comments.models import CommentFlag
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.comments import get_model as get_comment_model
+
+from django_comments.models import CommentFlag
+from django_comments import get_model as get_comment_model
 
 from tagging.models import Tag
 from tagging.utils import calculate_cloud
