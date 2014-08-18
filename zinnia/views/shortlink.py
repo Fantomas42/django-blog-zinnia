c0d38a75d279a9fd5610aee4694b169dff493ddc
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-08-18 17:42:44 +0200

    Add permission checking to shortlink
    In order to not leak details about an unpublished
    post via the post’s URL, shortlinks check permissions
    and return 404 if user is not allowed to see post.

--- zinnia/views/shortlink.py
+++ zinnia/views/shortlink.py
@@ -1,11 +1,25 @@
 """Views for Zinnia shortlink"""
-from django.shortcuts import get_object_or_404
+from django.http import Http404
 from django.views.generic.base import RedirectView
+from django.utils.translation import ugettext as _
 
 from zinnia.models.entry import Entry
+from zinnia.views.mixins.entry_preview import EntryPreviewMixin
 
 
-class EntryShortLink(RedirectView):
+class EntryShortLinkMixin(object):
+    """
+    Mixin implementing the preview of Entries.
+    """
+
+    def get_object(self, queryset=None):
+        try:
+            return queryset.get()
+        except Entry.DoesNotExist:
+            raise Http404(_('No entry found matching the query'))
+
+
+class EntryShortLink(RedirectView, EntryPreviewMixin, EntryShortLinkMixin):
     """
     View for handling the shortlink of an Entry,
     simply do a redirection.
@@ -17,5 +31,6 @@ class EntryShortLink(RedirectView):
         in the 'token' variable and return the get_absolute_url
         of the entry.
         """
-        entry = get_object_or_404(Entry, pk=int(kwargs['token'], 36))
+        pk = int(kwargs['token'], 36)
+        entry = self.get_object(Entry.objects.filter(pk=pk))
         return entry.get_absolute_url()
