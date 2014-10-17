5cd9cbed1ffb055e1dd160c42e4fcc89a9d32f56
Fantomas42 <fantomas42@gmail.com>
2014-10-17 16:20:24 +0200

    Do not declare the Author models in zinnia.models to avoid loading of this proxy models at django.setup(). The Author proxy model will be loaded on demand, like when loading zinnia.urls , close #342

--- zinnia/models/__init__.py
+++ zinnia/models/__init__.py
@@ -2,7 +2,6 @@
 from django_comments.moderation import moderator
 
 from zinnia.models.entry import Entry
-from zinnia.models.author import Author
 from zinnia.models.category import Category
 
 from zinnia.signals import connect_entry_signals
@@ -13,8 +12,8 @@ from zinnia.moderator import EntryCommentModerator
 # Here we import the Zinnia's Model classes
 # to register the Models at the loading, not
 # when the Zinnia's URLs are parsed. Issue #161.
+# Issue #161, seems not valid since Django 1.7.
 __all__ = [Entry.__name__,
-           Author.__name__,
            Category.__name__]
 
 # Register the comment moderator on Entry
