a85e910b57a920ae10e5d30be4d32614c8e87268
Fantomas42 <fantomas42@gmail.com>
2014-10-17 20:27:58 +0200

    Test migrations for Py2 & Py3

--- zinnia/migrations/0001_initial.py
+++ zinnia/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('level', models.PositiveIntegerField(editable=False, db_index=True)),
-                ('parent', mptt.fields.TreeForeignKey(related_name=b'children', verbose_name='parent category', blank=True, to='zinnia.Category', null=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', verbose_name='parent category', blank=True, to='zinnia.Category', null=True)),
             ],
             options={
                 'ordering': ['title'],
@@ -44,7 +44,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=255, verbose_name='title')),
-                ('slug', models.SlugField(help_text="Used to build the entry's URL.", max_length=255, verbose_name='slug', unique_for_date=b'creation_date')),
+                ('slug', models.SlugField(help_text="Used to build the entry's URL.", max_length=255, verbose_name='slug', unique_for_date='creation_date')),
                 ('status', models.IntegerField(default=0, db_index=True, verbose_name='status', choices=[(0, 'draft'), (1, 'hidden'), (2, 'published')])),
                 ('start_publication', models.DateTimeField(help_text='Start date of publication.', null=True, verbose_name='start publication', db_index=True, blank=True)),
                 ('end_publication', models.DateTimeField(help_text='End date of publication.', null=True, verbose_name='end publication', db_index=True, blank=True)),
@@ -58,16 +58,16 @@ class Migration(migrations.Migration):
                 ('pingback_count', models.IntegerField(default=0, verbose_name='pingback count')),
                 ('trackback_count', models.IntegerField(default=0, verbose_name='trackback count')),
                 ('excerpt', models.TextField(help_text='Used for search and SEO.', verbose_name='excerpt', blank=True)),
-                ('image', models.ImageField(help_text='Used for illustration.', upload_to=b'uploads/zinnia', verbose_name='image', blank=True)),
+                ('image', models.ImageField(help_text='Used for illustration.', upload_to='uploads/zinnia', verbose_name='image', blank=True)),
                 ('featured', models.BooleanField(default=False, verbose_name='featured')),
                 ('tags', tagging.fields.TagField(max_length=255, verbose_name='tags', blank=True)),
                 ('login_required', models.BooleanField(default=False, help_text='Only authenticated users can view the entry.', verbose_name='login required')),
                 ('password', models.CharField(help_text='Protects the entry with a password.', max_length=50, verbose_name='password', blank=True)),
-                ('content_template', models.CharField(default=b'zinnia/_entry_detail.html', help_text="Template used to display the entry's content.", max_length=250, verbose_name='content template', choices=[(b'zinnia/_entry_detail.html', 'Default template')])),
-                ('detail_template', models.CharField(default=b'entry_detail.html', help_text="Template used to display the entry's detail page.", max_length=250, verbose_name='detail template', choices=[(b'entry_detail.html', 'Default template')])),
-                ('categories', models.ManyToManyField(related_name=b'entries', null=True, verbose_name='categories', to='zinnia.Category', blank=True)),
+                ('content_template', models.CharField(default='zinnia/_entry_detail.html', help_text="Template used to display the entry's content.", max_length=250, verbose_name='content template', choices=[('zinnia/_entry_detail.html', 'Default template')])),
+                ('detail_template', models.CharField(default='entry_detail.html', help_text="Template used to display the entry's detail page.", max_length=250, verbose_name='detail template', choices=[('entry_detail.html', 'Default template')])),
+                ('categories', models.ManyToManyField(related_name='entries', null=True, verbose_name='categories', to='zinnia.Category', blank=True)),
                 ('related', models.ManyToManyField(related_name='related_rel_+', null=True, verbose_name='related entries', to='zinnia.Entry', blank=True)),
-                ('sites', models.ManyToManyField(help_text='Sites where the entry will be published.', related_name=b'entries', verbose_name='sites', to='sites.Site')),
+                ('sites', models.ManyToManyField(help_text='Sites where the entry will be published.', related_name='entries', verbose_name='sites', to='sites.Site')),
             ],
             options={
                 'get_latest_by': 'creation_date',
@@ -91,7 +91,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='entry',
             name='authors',
-            field=models.ManyToManyField(related_name=b'entries', verbose_name='authors', to='zinnia.Author', blank=True),
+            field=models.ManyToManyField(related_name='entries', verbose_name='authors', to='zinnia.Author', blank=True),
             preserve_default=True,
         ),
         migrations.AlterIndexTogether(
