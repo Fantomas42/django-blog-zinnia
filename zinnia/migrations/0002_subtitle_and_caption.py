4c0c2289ee8deeb6ea7f8bb76db886329e7ae1b3
Fantomas42 <fantomas42@gmail.com>
2015-01-07 15:14:44 +0100

    Add migration for adding subtitle and caption fields

---
+++ zinnia/migrations/0002_subtitle_and_caption.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('zinnia', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='entry',
+            name='caption',
+            field=models.TextField(
+                default='', help_text="Image's caption",
+                verbose_name='caption', blank=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='subtitle',
+            field=models.TextField(
+                default='', verbose_name='subtitle', blank=True),
+            preserve_default=False,
+        ),
+    ]
