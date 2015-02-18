108fa31a6a875940b6f0c3ebc1505ec97fa587dd
mavriq <mishanq@gmail.com>
2015-02-18 03:11:57 +0200

    force the migration to consider various combinations in ZINNIA_ENTRY_BASE_MODEL

--- zinnia/migrations/0002_lead_paragraph_and_image_caption.py
+++ zinnia/migrations/0002_lead_paragraph_and_image_caption.py
@@ -1,28 +1,31 @@
 from django.db import models
 from django.db import migrations
 
+from ..models import Entry
+from ..models_bases.entry import LeadEntry, ImageEntry
+
+operations = []
+if issubclass(Entry, ImageEntry):
+    operations.append(migrations.AddField(
+        model_name='entry',
+        name='image_caption',
+        field=models.TextField(
+            default='', help_text="Image's caption",
+            verbose_name='caption', blank=True),
+        preserve_default=False,))
+if issubclass(Entry, LeadEntry):
+    operations.append(migrations.AddField(
+        model_name='entry',
+        name='lead',
+        field=models.TextField(
+            default='', help_text='Lead paragraph',
+            verbose_name='lead', blank=True),
+        preserve_default=False))
+
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('zinnia', '0001_initial'),
     ]
 
-    operations = [
-        migrations.AddField(
-            model_name='entry',
-            name='image_caption',
-            field=models.TextField(
-                default='', help_text="Image's caption",
-                verbose_name='caption', blank=True),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='entry',
-            name='lead',
-            field=models.TextField(
-                default='', help_text='Lead paragraph',
-                verbose_name='lead', blank=True),
-            preserve_default=False,
-        ),
-    ]
+    operations = operations
