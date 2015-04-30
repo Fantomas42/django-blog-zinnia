43bbc933a7d609242dd227cfc8ebad256b3e5e46
Fantomas42 <fantomas42@gmail.com>
2015-04-30 20:02:42 +0200

    Compatibility fix for Django 1.7 & 1.8

--- zinnia/tests/utils.py
+++ zinnia/tests/utils.py
@@ -13,7 +13,7 @@ from django.utils import six
 from django.conf import settings
 from django.utils import timezone
 from django.test.client import Client
-from django.template.loaders.base import Loader
+from django.template.loader import BaseLoader
 
 
 class TestTransport(Transport):
@@ -71,23 +71,25 @@ def urlEqual(url_1, url_2):
             parse_qs(parse_result_1[5]) == parse_qs(parse_result_2[5]))
 
 
-class VoidLoader(Loader):
+class VoidLoader(BaseLoader):
     """
     Template loader which is always returning
     an empty template.
     """
     is_usable = True
+    _accepts_engine_in_init = True
 
     def load_template_source(self, template_name, template_dirs=None):
         return ('', 'voidloader:%s' % template_name)
 
 
-class EntryDetailLoader(Loader):
+class EntryDetailLoader(BaseLoader):
     """
     Template loader which only return the content
     of an entry detail template.
     """
     is_usable = True
+    _accepts_engine_in_init = True
 
     def load_template_source(self, template_name, template_dirs=None):
         return ('<html><head><title>{{ object.title }}</title></head>'
