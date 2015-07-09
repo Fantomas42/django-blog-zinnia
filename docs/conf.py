1b7f794ad060470a081456dacd9715fec12c4d64
Fantomas42 <fantomas42@gmail.com>
2015-07-09 17:07:08 +0200

    Use the classic new theme name

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # For using default theme on RTFD
 html_style = 'default.css'
