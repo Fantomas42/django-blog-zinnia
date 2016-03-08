61fbac0ca25921301ced42a6a1c66cf246621dcc
Fantomas42 <fantomas42@gmail.com>
2016-03-08 15:40:41 +0100

    Use BaseCommand instead of NoArgsCommand, thanks to @bittner, close #460

--- zinnia/management/commands/spam_cleanup.py
+++ zinnia/management/commands/spam_cleanup.py
@@ -1,20 +1,22 @@
-"""Spam cleanup command module for Zinnia"""
+"""
+Spam cleanup command module for Zinnia.
+"""
+from django.core.management.base import BaseCommand
 from django.contrib.contenttypes.models import ContentType
-from django.core.management.base import NoArgsCommand
 
 import django_comments as comments
 
 from zinnia.models.entry import Entry
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     """
     Command object for removing comments
     marked as non-public and removed.
     """
     help = "Delete the entries's comments marked as non-public and removed."
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         verbosity = int(options.get('verbosity', 1))
 
         content_type = ContentType.objects.get_for_model(Entry)
