1182a0d54db4d901acdfafc0e56f47d9512fbde6
Fantomas42 <fantomas42@gmail.com>
2016-03-16 14:56:35 +0100

    Define on_delete argument

---
+++ zinnia/migrations/0004_on_delete_arg.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db import migrations
+import mptt.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('zinnia', '0003_publication_date'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='category',
+            name='parent',
+            field=mptt.fields.TreeForeignKey(
+                blank=True, null=True,
+                on_delete=models.SET_NULL,
+                related_name='children', to='zinnia.Category',
+                verbose_name='parent category'),
+        ),
+    ]
