90cad29a45a82f3441cd470a6ba21246891ca49e
Fantomas42 <fantomas42@gmail.com>
2016-09-27 15:46:51 +0200

    Plug the signals when the apps are ready

--- zinnia/apps.py
+++ zinnia/apps.py
@@ -10,3 +10,17 @@ class ZinniaConfig(AppConfig):
     name = 'zinnia'
     label = 'zinnia'
     verbose_name = _('Weblog')
+
+    def ready(self):
+        from django_comments.moderation import moderator
+
+        from zinnia.signals import connect_entry_signals
+        from zinnia.signals import connect_discussion_signals
+        from zinnia.moderator import EntryCommentModerator
+
+        Entry = self.get_model('Entry')
+        # Register the comment moderator on Entry
+        moderator.register(Entry, EntryCommentModerator)
+        # Connect the signals
+        connect_entry_signals()
+        connect_discussion_signals()
