c9117b6b363c22013f976a1b7d7aa4d17ca71f47
Fantomas42 <fantomas42@gmail.com>
2017-03-03 00:29:57 +0100

    Specify the supported Python versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
