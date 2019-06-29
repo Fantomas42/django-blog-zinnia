5ab4afe15c284eb56f8dce3d416e3db8422c1ff6
Tayyeb <tayyeb@tsaze.com>
2019-06-29 23:37:43 +0430

    fix for flake8

--- zinnia/models_bases/category.py
+++ zinnia/models_bases/category.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
 from mptt.managers import TreeManager
 from mptt.models import MPTTModel
 from mptt.models import TreeForeignKey
