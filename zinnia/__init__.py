9cc1c552ed9b3f41a3bc08113b795be5bebdc450
elarroba <msanda@arrobalytics.com>
2021-12-07 20:29:08 -0500

    v0.3-alpha1

--- zinnia/__init__.py
+++ zinnia/__init__.py
@@ -1,10 +1,10 @@
 """Zinnia"""
-__version__ = '0.20.1.dev0'
+__version__ = 'v0.3-alpha1'
 __license__ = 'BSD License'
 
 __author__ = 'Fantomas42'
 __email__ = 'fantomas42@gmail.com'
 
-__url__ = 'https://github.com/Fantomas42/django-blog-zinnia'
+__url__ = 'https://github.com/arrobalytics/django-blog-zinnia'
 
 default_app_config = 'zinnia.apps.ZinniaConfig'
